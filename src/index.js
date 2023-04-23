0226c1fba54d9c99c1d52dc87fce96c7081a15c7
Amrit Sinha <56241123+SinhaAmrit@users.noreply.github.com>
2023-04-23 09:13:57 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,6 +7,6 @@ root.render(
   <React.StrictMode>
     <h1>Tic-Tac-Toe</h1>
     <App />
-    <div class="footer">Made with ❤ by <a href="https://github.com/SinhaAmrit/tic-tac-toe-two-player/" target="_blank" rel="noopener noreferrer">Amrit Sinha</a> &nbsp;&nbsp;|&nbsp;&nbsp; <a href="https://tic-tac-toe-single-player.vercel.app/" target="_blank" rel="noopener noreferrer">Try Single Player Mode 🧠</a> </div>
+    <div class="footer">Made with ❤ by <a href="https://github.com/SinhaAmrit/tic-tac-toe-two-player/" target="_blank" rel="noopener noreferrer">Amrit</a> &nbsp;&nbsp;|&nbsp;&nbsp; <a href="https://tic-tac-toe-single-player.vercel.app/" target="_blank" rel="noopener noreferrer">Single Player Mode 🧠</a> </div>
   </React.StrictMode>
 );
